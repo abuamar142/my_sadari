5fa672db51df10aba153c0a399354610305090ed
Abu Amar <abuamar.albadawi@gmail.com>
2025-06-19 06:31:17 +0700

    sign-in: revamp ui

--- lib/app/modules/sign_in/views/sign_in_view.dart
+++ lib/app/modules/sign_in/views/sign_in_view.dart
@@ -1,9 +1,10 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:my_sadari/app/styles/app_dimension.dart';
-import 'package:my_sadari/app/styles/app_text_style.dart';
 
 import '../../../styles/app_colors.dart';
+import '../../../styles/app_dimension.dart';
+import '../../../styles/app_text_style.dart';
 import '../controllers/sign_in_controller.dart';
 
 class SignInView extends GetView<SignInController> {
@@ -15,124 +16,167 @@ class SignInView extends GetView<SignInController> {
       decoration: BoxDecoration(gradient: AppColors.background1),
       child: Scaffold(
         backgroundColor: Colors.transparent,
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Center(
-              child: Container(
-                padding: EdgeInsets.all(AppDimensions.paddingLarge),
-                margin: EdgeInsets.all(AppDimensions.paddingLarge),
-                width: Get.width,
-                decoration: BoxDecoration(
-                  color: AppColors.white,
-                  borderRadius: BorderRadius.circular(AppDimensions.radiusLarge),
-                ),
-                child: Column(
-                  children: [
-                    Text(
-                      "Login",
-                      style: TextStyle(
-                        color: AppColors.pink,
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
+        body: Center(
+          child: SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.all(AppDimensions.paddingLarge),
+              margin: EdgeInsets.symmetric(
+                horizontal: AppDimensions.paddingLarge,
+              ),
+              width: Get.width * 0.9,
+              decoration: BoxDecoration(
+                color: AppColors.white,
+                borderRadius: BorderRadius.circular(AppDimensions.radiusLarge),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withValues(alpha: 0.1),
+                    blurRadius: 10,
+                    offset: Offset(0, 5),
+                  ),
+                ],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(height: 12),
 
-                    Row(
-                      children: [
-                        SizedBox(width: 16),
-                        Flexible(
-                          child: TextField(
-                            controller: controller.emailController,
-                            style: AppTextStyle.bodyMedium1,
-                            decoration: InputDecoration(
-                              hintText: 'Email',
-                              hintStyle: AppTextStyle.caption,
-                            ),
+                  Text(
+                    "Selamat Datang!",
+                    style: TextStyle(
+                      color: AppColors.black,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+
+                  SizedBox(height: 8),
+
+                  Text(
+                    "Masuk untuk melanjutkan",
+                    style: AppTextStyle.caption.copyWith(color: Colors.grey),
+                  ),
+
+                  SizedBox(height: 24),
+
+                  // Email Input
+                  TextField(
+                    controller: controller.emailController,
+                    style: AppTextStyle.bodyMedium1,
+                    decoration: InputDecoration(
+                      hintText: 'Email',
+                      hintStyle: AppTextStyle.caption,
+                      filled: true,
+                      fillColor: Colors.grey.withValues(alpha: 0.1),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(
+                          AppDimensions.radiusMedium,
+                        ),
+                        borderSide: BorderSide.none,
+                      ),
+                      prefixIcon: Icon(Icons.email, color: Colors.grey),
+                    ),
+                  ),
+                  SizedBox(height: 16),
+
+                  // Password Input
+                  Obx(
+                    () => TextField(
+                      controller: controller.passwordController,
+                      obscureText: controller.hidePassword.value,
+                      style: AppTextStyle.bodyMedium1,
+                      decoration: InputDecoration(
+                        hintText: 'Kata Sandi',
+                        hintStyle: AppTextStyle.caption,
+                        filled: true,
+                        fillColor: Colors.grey.withValues(alpha: 0.1),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(
+                            AppDimensions.radiusMedium,
+                          ),
+                          borderSide: BorderSide.none,
+                        ),
+                        prefixIcon: Icon(Icons.lock, color: Colors.grey),
+                        suffixIcon: IconButton(
+                          onPressed: controller.toggleVisibility,
+                          icon: Icon(
+                            controller.hidePassword.value
+                                ? Icons.visibility
+                                : Icons.visibility_off,
+                            color: Colors.grey,
                           ),
                         ),
-                      ],
-                    ),
-                    SizedBox(height: 16),
-                    Row(
-                      children: [
-                        SizedBox(width: 16),
-                        Flexible(
-                          child: Obx(
-                            () => TextField(
-                              controller: controller.passwordController,
-                              obscureText: controller.hidePassword.value,
-                              decoration: InputDecoration(
-                                hintText: '********',
-                                hintStyle: AppTextStyle.caption,
-                                suffixIcon: IconButton(
-                                  onPressed: controller.toggleVisibility,
-                                  // icon: SvgPicture.asset(
-                                  //   controller.hidePassword.value ? iconC.broEye : iconC.broEyeSlash,
-                                  //   height: 24,
-                                  //   colorFilter: ColorFilter.mode(
-                                  //     AppColors.black,
-                                  //     BlendMode.srcIn,
-                                  //   ),
-                                  // ),
-                                  icon: Icon(
-                                    controller.hidePassword.value
-                                        ? Icons.visibility
-                                        : Icons.visibility_off,
-                                  ),
-                                ),
-                              ),
-                              style: AppTextStyle.bodyMedium1,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 8),
-
-                    Align(
-                      alignment: Alignment.centerRight,
-                      child: TextButton(
-                        onPressed: () {},
-                        child: Text(
-                          'Forgot Password?',
-                          style: TextStyle(color: AppColors.black),
-                        ),
                       ),
                     ),
+                  ),
 
-                    SizedBox(height: 20),
+                  SizedBox(height: 4),
 
-                    SizedBox(
-                      width: double.infinity,
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: AppColors.pink,
-                          padding: EdgeInsets.symmetric(vertical: 16),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                        ),
-                        onPressed: () {},
-                        child: Text('LOGIN', style: AppTextStyle.buttonText1),
-                      ),
-                    ),
-
-                    SizedBox(height: 20),
-                    Text('New User?'),
-
-                    TextButton(
+                  // Forgot Password
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: TextButton(
                       onPressed: () {},
                       child: Text(
-                        'Register Here',
-                        style: TextStyle(color: AppColors.pink),
+                        'Lupa Kata Sandi?',
+                        style: TextStyle(
+                          color: AppColors.pink,
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
-                  ],
-                ),
+                  ),
+
+                  // Login Button
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: AppColors.pink,
+                        padding: EdgeInsets.symmetric(vertical: 12),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(
+                            AppDimensions.radiusMedium,
+                          ),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: Text('MASUK', style: AppTextStyle.buttonText1),
+                    ),
+                  ),
+
+                  SizedBox(height: 20),
+
+                  // Register Section
+                  RichText(
+                    textAlign: TextAlign.center,
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: 'Pengguna Baru? ',
+                          style: AppTextStyle.caption.copyWith(
+                            color: Colors.grey,
+                          ),
+                        ),
+                        TextSpan(
+                          text: 'Daftar Disini',
+                          style: AppTextStyle.caption.copyWith(
+                            color: AppColors.pink,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          recognizer:
+                              TapGestureRecognizer()
+                                ..onTap = () {
+                                  Get.toNamed('/sign-up');
+                                },
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
-          ],
+          ),
         ),
       ),
     );
